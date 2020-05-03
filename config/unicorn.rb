ce6108c454f293088227e657b145e5cbf67e2954
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-05-03 13:05:38 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,8 +5,8 @@ worker_processes 1
 working_directory "#{app_path}/current"
 
 # それぞれ、sharedの中を参照するよう変更
-# listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-listen 3000
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+# listen 3000
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
