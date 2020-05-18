701a7eb21d133f6f6cab465433faab04f7a68ac5
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-05-18 11:33:39 +0900

    userが登録されていない場合は電話番号認証ページ以降に飛べないように

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   prepend_before_action :check_recaptcha, only: [:create]
+  before_action :authenticate_scope!, only: [:confirm_phone, :new_address, :create_address]
   layout 'no_menu'
 
   # GET /resource/sign_up
