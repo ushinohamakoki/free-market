d26bd10ce901d20010d6752f5830b133db622900
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-05-22 15:29:53 +0900

    itemモデルとprefectureモデルのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,9 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: :true
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
   validates :name, :price, :detail, :condition, :delivery_fee_payer, :delivery_method, :delivery_days, :deal, presence: true
   validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
