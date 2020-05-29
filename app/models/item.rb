60679479ce746621b74ec40b296a2c1a80e294b0
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-05-29 14:18:17 +0900

    モデルファイルにaccepts_nested_attributes_forを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,8 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: :true
-  has_many :images, dependent: :destroy
+  has_many :images
+  accepts_nested_attributes_for :images
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
