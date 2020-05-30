d84d65c2e5c6649cba2e2488e882535591bee21e
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-05-30 16:38:53 +0900

    何番目の画像を取り扱っているのか判定

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -1,6 +1,17 @@
 document.addEventListener('turbolinks:load', function () {
 
   if (!$('#item_form')[0]) return false;
+
+  function newFileField(index) {
+
+    const html = `
+                  <input accept="image/*" class="new-item-image" style="display: block;" data-index="${index}" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
+                  `;
+    return html;
+
+  }
+
+
   $("#select-image-button").on("click", function () {
     const file_field = $("#item_images_attributes_0_src");
     file_field.trigger("click"); 
@@ -9,13 +20,18 @@ document.addEventListener('turbolinks:load', function () {
   $("#image-file-fields").on("change", `input[type="file"]`, function (a) {
     console.table(a.target.files);
     console.log("画像が選択されました")
-
     const file = a.target.files[0];
+    let index = $(this).data("index");
+    console.log("選択した画像のindex=", index);
     const blob_url = window.URL.createObjectURL(file);
     console.log(blob_url);
-
     const preview_html = `<img src="${blob_url}" width="20%">`;
     $("#select-image-button").before(preview_html);
+
+    index += 1;
+    const file_field_html = newFileField(index);
+    $("#image-file-fields").append(file_field_html);
+    
   });
 
 
