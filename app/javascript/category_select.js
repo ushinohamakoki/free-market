c960271c563d233d8d17a267f41d27164f132230
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-06-06 17:41:17 +0900

    カテゴリが選択されたら発火するイベント

--- app/javascript/category_select.js
+++ app/javascript/category_select.js
@@ -1,4 +1,10 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('.select-category')[0]) return false;
   console.log("hoge");
+
+  $(".input-field-main").on("change", ".select-category", function () {
+    const category_id = $(this).val();
+    console.log("選択されたカテゴリのID:", category_id);
+  });
+
 });
