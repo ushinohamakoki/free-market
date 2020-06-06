f3c1beb40516e8b31a504e3453bc59ddfd06a154
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-06-06 17:45:40 +0900

    Update category_select.js

--- app/javascript/category_select.js
+++ app/javascript/category_select.js
@@ -5,6 +5,26 @@ document.addEventListener('turbolinks:load', function () {
   $(".input-field-main").on("change", ".select-category", function () {
     const category_id = $(this).val();
     console.log("選択されたカテゴリのID:", category_id);
-  });
 
+
+    $.ajax({
+      url: "/api/categories",
+      type: "GET",
+      data: { category_id: category_id },
+      dataType: 'json',
+    })
+
+    .done(function (categories) {
+      console.log("success")
+    })
+    .fail(function () {
+      alert('error');
+    })
+
+
+
+
+
+
+  });
 });
