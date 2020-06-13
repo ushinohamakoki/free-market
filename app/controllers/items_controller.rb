037206642b32cea6925a91d3f6baa5c882beed00
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-06-13 14:43:56 +0900

    商品の詳細ページはログイン不要に

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_item, only: [:edit, :update, :destroy]
   before_action :user_is_not_seller, only: [:edit, :update, :destroy]
 
