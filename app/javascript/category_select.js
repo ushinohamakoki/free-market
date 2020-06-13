109730848233ddfa677efa74c95b38fb6670a74c
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-06-13 15:22:31 +0900

    カテゴリーの修正

--- app/javascript/category_select.js
+++ app/javascript/category_select.js
@@ -1,13 +1,60 @@
-document.addEventListener('turbolinks:load', function () {
-  if (!$('.select-category')[0]) return false;
+// document.addEventListener('turbolinks:load', function () {
+//   if (!$('.select-category')[0]) return false;
 
-  function buildCategoryForm(categories) {
-    const options = "";
-    categories.forEach(function (category) {
+//   function buildCategoryForm(categories) {
+//     const options = "";
+//     categories.forEach(function (category) {
+//       options += `
+//                   <option value="${category.id}">${category.name}</option>
+//                  `;
+//     });
+//     const html = `
+//                   <select required="required" class="select-category" id="parent-category" name="item[category_id]">
+//                     <option value="">---</option>
+//                     ${options}
+//                   </select>
+//                  `;
+//     return html;
+//   }
+
+//   $(".input-field-main").on("change", ".select-category", function () {
+//     const category_id = $(this).val();
+//     console.log("選択されたカテゴリのID:", category_id);
+//     const changed_form = $(this);
+//     $.ajax({
+//       url: "/api/categories",
+//       type: "GET",
+//       data: { category_id: category_id },
+//       dataType: 'json',
+//     })
+
+//     .done(function (categories) {
+//       if (categories.length == 0) return false;
+//       changed_form.nextAll(".select-category").remove();
+//       console.table(categories);
+//       const html = buildCategoryForm(categories);
+//       console.log(html);
+//       $(".select-category:last").after(html);
+//     })
+
+//     .fail(function () {
+//       alert('error');
+//     })
+
+//   });
+// });
+
+document.addEventListener('turbolinks:load', function () {
+  if (!$('.select-category')[0]) return false; //カテゴリのフォームが無いなら以降実行しない。
+
+  function buildCategoryForm(categories) { // 子孫カテゴリのフォームを組み立てる
+    let options = "";
+    categories.forEach(function (category) { // カテゴリを一つずつ渡してoptionタグを一つずつ組み立てていく。
       options += `
                   <option value="${category.id}">${category.name}</option>
                  `;
     });
+
     const html = `
                   <select required="required" class="select-category" id="parent-category" name="item[category_id]">
                     <option value="">---</option>
@@ -16,30 +63,26 @@ document.addEventListener('turbolinks:load', function () {
                  `;
     return html;
   }
+  /////////buildCategoryForm()ここまで/////////
 
-  $(".input-field-main").on("change", ".select-category", function () {
-    const category_id = $(this).val();
-    console.log("選択されたカテゴリのID:", category_id);
-    const changed_form = $(this);
+  $(".input-field-main").on("change", ".select-category", function () { //カテゴリが選択された時
+    const category_id = $(this).val(); // 選択されたカテゴリのidを取得
+    var changed_form = $(this); // ajax後でもthisが使えるようにするため
     $.ajax({
-      url: "/api/categories",
-      type: "GET",
-      data: { category_id: category_id },
-      dataType: 'json',
-    })
-
-    .done(function (categories) {
-      if (categories.length == 0) return false;
-      changed_form.nextAll(".select-category").remove();
-      console.table(categories);
-      const html = buildCategoryForm(categories);
-      console.log(html);
-      $(".select-category:last").after(html);
-    })
-
-    .fail(function () {
-      alert('error');
-    })
-
+        url: "/api/categories",
+        type: "GET",
+        data: {
+          category_id: category_id
+        },
+        dataType: 'json',
+      }).done(function (categories) {
+        if (categories.length == 0) return false; // categoriesが空 = 選択されたのが孫カテゴリなら何もせず終了
+        changed_form.nextAll(".select-category").remove(); // 重複して表示させないために変更されたフォームより後ろのフォームを削除
+        const html = buildCategoryForm(categories); // カテゴリのフォームを組み立てる
+        $(".select-category:last").after(html); // 組み立てたフォームを表示
+      })
+      .fail(function () {
+        alert('error');
+      })
   });
 });
