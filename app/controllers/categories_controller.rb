a5164772b9195a13d20575fd3c5bcf8701b816d7
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-06-20 18:43:02 +0900

    カテゴリ毎の詳細ページで商品を表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @items = Item.search_by_categories(@category.subtree_ids)
   end
-  
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
+
+  def self.search_by_categories(category_ids)
+    return Item.where(category_id: category_ids).includes(:images)
+  end
+
   has_many :items
 
   has_ancestry
