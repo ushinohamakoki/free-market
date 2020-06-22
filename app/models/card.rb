0e78f675385817b62b0979273853093ca3a7f474
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-06-22 18:20:04 +0900

    アソシエーションの修正

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,3 @@
 class Card < ApplicationRecord
   belongs_to :user
-  has_one :card, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
    has_one :address, dependent: :destroy
    has_many :selling_items, class_name: "Item", foreign_key: "seller_id"
    has_many :bought_items, class_name: "Item", foreign_key: "buyer_id"
+   has_one :card, dependent: :destroy
    
    def self.from_omniauth(auth_data)
     email = auth_data.info.email
