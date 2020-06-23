0f7ef1118f64b6fc694a16c441149fc9ffcc33cf
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-06-23 15:30:58 +0900

    要らない文字の削除

--- app/javascript/regist_card.js
+++ app/javascript/regist_card.js
@@ -1,10 +1,9 @@
 document.addEventListener('turbolinks:load', function () {
-  if (!$('#card_form')[0]) return false; //カード登録ページではないなら以降実行しない。
+  if (!$('#card_form')[0]) return false;
 
-  Payjp.setPublicKey("pk_test_89b36770be8d15264122f4db"); //公開鍵を読み込む。
-  const regist_button = $("#regist_card"); //カード入力フォームの登録ボタン。
-  regist_button.on("click", function (e) { //登録ボタンを押したとき（ここはsubmitではなくclickにしておく）。
-    e.preventDefault();
+  Payjp.setPublicKey("pk_test_89b36770be8d15264122f4db");
+  const regist_button = $("#regist_card");
+  regist_button.on("click", function (e) {
 
     const card = {
       number: $("#card_number_form").val(),
@@ -13,13 +12,10 @@ document.addEventListener('turbolinks:load', function () {
       exp_year: $("#exp_year_form").val()
     };
 
-    Payjp.createToken(card, (status, response) => { //cardをpayjpに送信して登録する。
-      console.log("aa")
-      if (status === 200) { //成功した場合
+    Payjp.createToken(card, (status, response) => {
+      if (status === 200) { 
         alert("カードを登録しました");
 
-        // 追加ここから
-        // ↓hidden_fieldにcardのtokenを入れることでtokenがparamsに送られる。
         $("#card_token").append(
           `<input type="hidden" name="payjp_token" value=${response.id}>
           <input type="hidden" name="card_token" value=${response.card.id}>`
@@ -29,7 +25,7 @@ document.addEventListener('turbolinks:load', function () {
         $("#cvc_form").removeAttr("name");
         $("#exp_month_form").removeAttr("name");
         $("#exp_year_form").removeAttr("name");
-      } else { //失敗した場合
+      } else { 
         alert("カード情報が正しくありません。");
         regist_button.prop('disabled', false);
       }
