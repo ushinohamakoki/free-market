1bb8687387980f5954ddd12e56e334dbfe37500e
ushinohamakoki <60771575+ushinohamakoki@users.noreply.github.com>
2020-06-23 15:38:31 +0900

    コントローラーでdefault_cardとしてカード情報を取得

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,7 +13,7 @@ class CardsController < ApplicationController
     Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
 
     customer = Payjp::Customer.create(card: params[:payjp_token]) ## 顧客の作成
-    card = current_user.build_card(card_token: params[:card_token], customer_token: customer.id)
+    card = current_user.build_card(card_token: customer.default_card, customer_token: customer.id)
     if card.save
 
       redirect_to cards_path, notice: "カードの登録が完了しました。"
